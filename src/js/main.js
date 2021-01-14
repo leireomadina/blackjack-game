bd209abb06d98c8a2036523c12258b02e9f124f5
Leire Ordeñana <leireomadina@gmail.com>
2021-01-14 13:56:24 +0100

    Add create deck and shuffle logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,39 @@
 "use strict";
 
-console.log(">> Ready :)");
+let deck = [];
+
+// Creating deck cards
+const cardTypes = ["C", "D", "H", "S"], // C: Clubs, D: Diamonds, H: Hearts, S: Spades
+  specialCards = ["J", "Q", "K", "A"];
+
+/* Function to shuffle the deck: this loop iterates over an array from back to front bypassing index 0. Each iteration generates a random number (index variable) ranging between 0 and the counter variable (last item), and then swaps both values to suffle them.*/
+const shuffleDeck = (array) => {
+  let counter = array.length;
+  while (counter > 0) {
+    let index = Math.floor(Math.random() * counter);
+
+    counter--;
+
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+  return array;
+};
+
+const createDeck = () => {
+  deck = [];
+  for (let cardType of cardTypes) {
+    for (let i = 2; i <= 10; i++) {
+      deck.push(i + cardType);
+    }
+    for (let specialCard of specialCards) {
+      deck.push(specialCard + cardType);
+    }
+  }
+  shuffleDeck(deck);
+  return deck; 
+};
+
+createDeck();
+console.log(deck);
